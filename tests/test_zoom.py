c94bee1694208a0300bb9b93664dbdfc58981ae1
CaoTianze <hnrcao@qq.com>
2023-08-13 16:56:27 +0800

    Update test_zoom.py

--- tests/test_zoom.py
+++ tests/test_zoom.py
@@ -1,11 +1,11 @@
-from plotnine import ggplot, coord_fixed, ggtitle, theme, element_text, scale_x_continuous
-from plotnineseqsuite.align import geom_alignedSeq, theme_seq, extract
+from plotnine import ggplot, coord_fixed, ggtitle, scale_x_continuous
+from plotnineseqsuite import geom_alignedSeq, theme_seq, extract
 from math import floor, ceil
 
 seqs = ['TTGTGAAAGAC', 'AAGTAAACTAA', 'TAATAAACAAA', 'TAATAAACAAA', 'CTGTAAATATT', 'TAGAAAGGTAT']
 result = extract(seqs, start=1, end=4)
-a=ggplot() + geom_alignedSeq(data=seqs) + coord_fixed() + theme_seq()+ggtitle('Completed sequences') + theme(plot_title=element_text(size=30, ha='left'))
-b=ggplot() + geom_alignedSeq(data=result) + scale_x_continuous(breaks=lambda x: range(floor(x[0]), ceil(x[1])), labels=lambda x:x+1, expand=(0,0)) + coord_fixed() + theme_seq()+ggtitle('Fragmented sequences') + theme(plot_title=element_text(size=30, ha='left'))
+a=ggplot() + geom_alignedSeq(data=seqs) + coord_fixed() + theme_seq()+ggtitle('Completed sequences')
+b=ggplot() + geom_alignedSeq(data=result) + scale_x_continuous(breaks=lambda x: range(floor(x[0]), ceil(x[1])), labels=lambda x: range(x[0]+1, x[0]+1+len(x)), expand=(0,0)) + coord_fixed() + theme_seq()+ggtitle('Fragmented sequences')
 a.save('Fig. complete.png')
 b.save('Fig. fragment.png')
 
