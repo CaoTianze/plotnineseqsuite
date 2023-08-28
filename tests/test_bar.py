a02f2a06e8902fd07283156801ca6ddbfbdac9e7
CaoTianze <hnrcao@qq.com>
2023-08-28 11:03:22 +0800

    Update test_bar.py

--- tests/test_bar.py
+++ tests/test_bar.py
@@ -1,5 +1,5 @@
 from numpy import array
-from plotnine import ggplot
+from plotnine import ggplot, theme_void
 
 from plotnineseqsuite.bar import geom_seqBar
 from plotnineseqsuite.theme import theme_seq
@@ -10,12 +10,15 @@ def test_make_max_frequency():
     a=geom_seqBar(seqs)._geom_seqbar__make_max_frequency(seqs)
     assert (a['max_frequency_mat'][0] == array(['T','A','G','T','A','A','A','C','A','A','A'])).all()
 
+
 def test_max_frequency_method():
     seqs = ['TTGTGAAAGAC', 'AAGTAAACTAA', 'TAATAAACAAA', 'TAATAAACAAA', 'CTGTAAATATT', 'TAGAAAGGTAT']
     a = geom_seqBar(seqs)._geom_seqbar__max_frequency_method(seqs, seq_type='AUTO', namespace=None)
     assert a['frequency_df'].size == 11*3
 
+
 def test_geom_seqBar():
     seqs = ['TTGTGAAAGAC', 'AAGTAAACTAA', 'TAATAAACAAA', 'TAATAAACAAA', 'CTGTAAATATT', 'TAGAAAGGTAT']
     ggplot() + geom_seqBar(seqs) + theme_seq()
     ggplot() + geom_seqBar(seqs, font=None) + theme_seq()
+    ggplot() + geom_seqBar(seqs)+ theme_void()
