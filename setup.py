2663faa32bad0583ae1e4f457e4461aa2e98f4ba
CaoTianze <hnrcao@qq.com>
2024-12-13 13:55:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="plotnineseqsuite",
-    version="1.1.2",
+    version="1.1.3",
     author="Cao Tianze",
     author_email="hnrcao@qq.com",
     description="A Python package for visualizing sequence data using ggplot2 style",
